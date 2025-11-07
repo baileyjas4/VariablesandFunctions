ee8e2832e583c2c2763e97551ef655ec9b22d918
baileyjas4 <baileyjasmine4@icloud.com>
2025-11-07 15:49:20 -0500

    task2

--- script.js
+++ script.js
@@ -7,4 +7,30 @@ console.log(fullname ("Jasmine", "Bailey"))
 
 //TASK 1 
 
+function calculateTotalCost(Price, Quantity, Taxrate){
+    if(Price,   Quantity,    Taxrate)
+    return "Wrong Input";
+return (Price * Quantity) * (1 + Taxrate);
+console.log(calculateTotalCost)
+}
+
+//TASK 2
+
+
+let age = "true";
+
+ if (age >= 16){
+    console.log("you are old enough to drive!")
+ }
+
+ if (age >= 21){
+    console.log("You are old enough to drink")
+ } else if (age < 21) {
+   console.log("You are too young to drink")
+} else {
+    console.log(" either you are too young, too old, or dont exist")}
+
+
+    //TASK 3
+
 
